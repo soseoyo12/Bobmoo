007f4791d3a6527d4679803a43cee44da7361b5d
hwoo7449 <hwoo7449@naver.com>
2025-12-05 20:58:59 +0900

    [Style] #14 - 메뉴 폰트 스타일 조정

--- BobmooAnd/lib/widgets/meal_item_row.dart
+++ BobmooAnd/lib/widgets/meal_item_row.dart
@@ -1,5 +1,7 @@
+import 'package:bobmoo/constants/app_colors.dart';
 import 'package:bobmoo/models/menu_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MealItemRow extends StatelessWidget {
   final MealItem meal;
@@ -8,39 +10,50 @@ class MealItemRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 1.5),
+      padding: EdgeInsets.zero,
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // 1. 코스 (A, B, C...)
-          SizedBox(
-            width: 15, // 너비를 고정하여 정렬을 맞춤
-            child: Text(
-              meal.course,
-              style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-            ),
+          Text(
+            "${meal.course} ",
+            style: TextStyle(fontSize: 15.sp, fontWeight: FontWeight.w500),
           ),
-          // 2. 메뉴 이름 (가장 넓은 공간 차지)
+          // 2. 메뉴 이름
           Expanded(
-            child: Text(
-              meal.mainMenu,
-              style: const TextStyle(fontSize: 14),
-              // TODO: 메인 메뉴 한줄로 하고 ...을 뒤에 붙히도록 할지 고민
-              // maxLines: 1,
-              // overflow: TextOverflow.ellipsis,
+            // Warp 위젯을 통해 단어 별로 묶은다음에, 잘리게 된다면 다음줄로 넘어가게
+            child: Wrap(
+              spacing: 0, // 단어 사이 가로 간격
+              runSpacing: 0, // 줄 사이 세로 간격
+              children: meal.mainMenu
+                  .split(' ') // 쉼표로 분리
+                  .asMap()
+                  .entries
+                  .map((entry) {
+                    return Text(
+                      "${entry.value} ",
+                      style: TextStyle(
+                        fontSize: 15.sp,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    );
+                  })
+                  .toList(),
             ),
-          ),
-          SizedBox(
-            width: 4,
           ),
           Padding(
-            padding: const EdgeInsets.only(top: 2),
+            padding: EdgeInsets.zero,
             child: Text(
               "${meal.price}원",
               style: TextStyle(
-                fontSize: 12,
-                fontWeight: FontWeight.w500,
-                color: Colors.grey.shade500,
+                fontSize: 11.sp,
+                fontFamily: 'NanumSquareRound',
+                fontWeight: FontWeight.w700,
+                color: AppColors.greyTextColor,
+                // 자간 5%
+                letterSpacing: 11.sp * 0.02,
+                // 행간 21px
+                height: 2.0,
               ),
             ),
           ),
