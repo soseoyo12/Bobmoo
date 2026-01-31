177a7345a2d04472764713325fa81df2ba95bb46
hwoo7449 <hwoo7449@naver.com>
2026-02-01 05:03:53 +0900

    [Feat] #21 - locator에 sharedPreferences 등록

--- BobmooAnd/lib/locator.dart
+++ BobmooAnd/lib/locator.dart
@@ -7,11 +7,16 @@ import 'package:bobmoo/services/menu_service.dart';
 import 'package:get_it/get_it.dart';
 import 'package:isar_community/isar.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final locator = GetIt.instance;
 
 Future<void> setupLocator() async {
-  // 1. Isar 인스턴스 초기화 및 등록
+  // SharedPreferences 등록
+  final prefs = await SharedPreferences.getInstance();
+  locator.registerSingleton<SharedPreferences>(prefs);
+
+  // Isar 인스턴스 초기화
   final dir = await getApplicationDocumentsDirectory();
   final isar = await Isar.open(
     [
@@ -24,11 +29,11 @@ Future<void> setupLocator() async {
   // Isar 인스턴스를 싱글톤으로 등록
   locator.registerSingleton<Isar>(isar);
 
-  // 2. 서비스(API) 클래스 등록
+  // 서비스(API) 클래스 등록
   // registerLazySingleton: 해당 클래스를 처음 요청할 때 인스턴스가 생성됨
   locator.registerLazySingleton(() => MenuService());
 
-  // 3. Repository 등록
+  // Repository 등록
   // MealRepository는 Isar와 MenuService를 필요로 하므로, locator를 통해 주입받음
   locator.registerLazySingleton(
     () => MealRepository(
